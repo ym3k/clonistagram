41c3fdfda4d93039d3ca3175c89531a52f838b85
Tomoyuki Yamazaki <yama3@nttpc.co.jp>
2018-01-21 04:47:54 +0000

    hostname updated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host: 'peaceful-escarpment-91156.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'glacial-everglades-14704.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
